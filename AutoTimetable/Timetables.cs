a27eb4f63650914dac3314baa9befc7b7266645e
Max P <Mail@MPassarello.de>
2024-08-29 14:52:09 +0200

    Template

---
+++ AutoTimetable/Timetables.cs
@@ -0,0 +1,16 @@
+namespace AutoTimetable
+{
+    public class Timetables
+    {
+        private string url;
+        private string className;
+
+        public Timetables(string url, string className)
+        {
+            this.url = url;
+            this.className = className;
+        }
+
+
+    }
+}
